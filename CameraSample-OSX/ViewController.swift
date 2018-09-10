04ea9d8277c6b27473cc6b3c0559f43ab6c30c7c
isaoeka <isaoeka@gmail.com>
2018-09-10 13:59:03 +0900

    :pencil2: Initial commit

---
+++ CameraSample-OSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CameraSample-OSX
+//
+//  Created by isaoeka on 2018/09/10.
+//  Copyright © 2018年 isaoeka. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
