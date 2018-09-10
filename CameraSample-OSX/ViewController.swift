74f8acf5734b38aa7b280d2ad3321f05d601389f
isaoeka <isaoeka@gmail.com>
2018-09-10 14:49:32 +0900

    :pencil2: Add camera view.

--- CameraSample-OSX/ViewController.swift
+++ CameraSample-OSX/ViewController.swift
@@ -7,21 +7,38 @@
 //
 
 import Cocoa
+import AVFoundation
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var cameraView: NSView!
+    
+    let captureSession = AVCaptureSession()
+    var captureDevice : AVCaptureDevice?
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+        
+        self.cameraView.layer = CALayer()
+        self.captureSession.sessionPreset = AVCaptureSession.Preset.low
+        
+        let devices = AVCaptureDevice.devices(for: AVMediaType.video)
+        dump(devices)
+        self.captureDevice = devices.first
+        
+        if let captureDevice = self.captureDevice {
+            do {
+                try self.captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
+                self.previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+                self.previewLayer?.frame = self.cameraView.frame
+                
+                self.cameraView.layer?.addSublayer(self.previewLayer!)
+                self.captureSession.startRunning()
+            } catch {
+                print(AVCaptureSessionErrorKey.description)
+            }
         }
     }
-
-
+    
 }
-
